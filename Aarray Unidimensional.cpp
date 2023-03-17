f391688990192f5fe054ecf3818aec07419b55f7
DanniHLJ <hljdanni2023@gmail.com>
2023-03-16 22:34:59 -0600

    Actividad 5

---
+++ Aarray Unidimensional.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+main (){
+	
+	// array = arreglo o colección o matriz
+	// se utilizan para almacenar varios valores en una sola variable 
+	
+string Semana[7]={"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"}; 
+	Semana [3]= "jueves";
+	for (int i =0; i<7;i++){
+		cout<<Semana[i]<<endl;
+	} 
+
+	
+	system ("pause");
+}
